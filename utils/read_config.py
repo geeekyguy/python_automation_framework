e9b9e4705634f5f2d23b3d2a5008618e73b250d0
geeekyguy <mukunramesh96@gmail.com>
2026-04-10 13:30:38 +0530

    Commit and Push

--- utils/read_config.py
+++ utils/read_config.py
@@ -1,6 +1,6 @@
+import os
 import yaml
 from utils.path_utils import CONFIG_DIR
-
 
 class ConfigReader:
     def __init__(self, env="qa"):
@@ -9,12 +9,16 @@ class ConfigReader:
 
     def _load_config(self):
         config_path = CONFIG_DIR / f"{self.env}.yaml"
-
-        if not config_path.exists():
-            raise FileNotFoundError(f"Config file not found at: {config_path}")
-
         with open(config_path, "r", encoding="utf-8") as file:
             return yaml.safe_load(file)
 
     def get(self, key, default=None):
+        env_map = {
+            "base_url": "BASE_URL",
+            "username": "APP_USERNAME",
+            "password": "APP_PASSWORD",
+        }
+        env_key = env_map.get(key)
+        if env_key and os.getenv(env_key):
+            return os.getenv(env_key)
         return self.config_data.get(key, default)
